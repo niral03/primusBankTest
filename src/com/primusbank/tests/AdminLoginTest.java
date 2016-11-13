b3cff74488371f12626813c182d401bc487b211b
demoqedge123 <demo.qedge@gmail.com>
2016-11-13 13:11:30 +0530

    Version 1

---
+++ src/com/primusbank/tests/AdminLoginTest.java
@@ -0,0 +1,18 @@
+package com.primusbank.tests;
+
+public class AdminLoginTest {
+
+	/*
+		Project Name	:	PrimusBank
+		Module Name		:	Admin
+		Script Desc		:	This Script validates Admin Login
+		Prepared By		:	xxxxxxxxxxxxxxxxxxx
+		Prepared Date	:	xx/xx/xxxx	
+	*/
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
